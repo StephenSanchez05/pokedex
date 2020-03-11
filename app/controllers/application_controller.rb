e6a99afdee6a662f992bbe8991781fca0912992a
Stephen Sanchez <stephensanchez05@gmail.com>
2020-03-11 00:32:42 +0000

    added helpers script

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
     end
   end
   
-  get "/ogin" do
+  get "/login" do
     erb :login
   end
   
@@ -44,5 +44,15 @@ class ApplicationController < Sinatra::Base
   get "/error" do
     erb :error
   end 
+  
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end 
+    
+    def current_user
+    User.find(session[:user_id])
+  end
+end 
 
 end
