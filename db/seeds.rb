6e7554390210269acc4eb281f79b98a296d18055
Stephen Sanchez <stephensanchez05@gmail.com>
2020-03-13 02:40:43 +0000

    deleted excess pokemon

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-Trainer.create(name: "Blue")
-Pokemon.create(name: "Squirtle")
-Pokemon.create(name: "Bulbasaur")
-Pokemon.create(name: "Charmeleon")
+#Trainer.create(name: "Blue")
+#Pokemon.create(name: "Squirtle")
+#Pokemon.create(name: "Bulbasaur")
+#Pokemon.create(name: "Charmeleon")
+Trainer.destroy_all
+Pokemon.destroy_all
