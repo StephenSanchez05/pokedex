b282e02cf76eca107364a44131685cd41e91268a
Stephen Sanchez <stephensanchez05@gmail.com>
2020-03-16 16:36:31 +0000

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UserController < ApplicationController
       redirect '/error'
     else
       @user = User.create(username: params[:username], password: params[:password])
+      @user.save
       session[:user_id] = @user.id
       redirect '/login'
     end
